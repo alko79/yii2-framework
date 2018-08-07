0892d5735aee16bb1084502c9b755166731c9bfe
alko79 <alessandro.mirabelli@gmx.com>
2018-08-07 10:12:17 +0200

    Update ActionColumn.php
    edit added instead of pencil

--- grid/ActionColumn.php
+++ grid/ActionColumn.php
@@ -143,7 +143,7 @@ class ActionColumn extends Column
     protected function initDefaultButtons()
     {
         $this->initDefaultButton('view', 'eye');
-        $this->initDefaultButton('update', 'pencil');
+        $this->initDefaultButton('update', 'edit');
         $this->initDefaultButton('delete', 'trash', [
             'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
             'data-method' => 'post',
