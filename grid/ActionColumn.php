9384504871cf8e7993aa5a991ceb0eec4ae4df16
alko79 <alessandro.mirabelli@gmx.com>
2019-06-21 10:02:35 +0200

    Update ActionColumn.php

--- grid/ActionColumn.php
+++ grid/ActionColumn.php
@@ -179,7 +179,7 @@ class ActionColumn extends Column
                     'aria-label' => $title,
                     'data-pjax' => '0',
                 ], $additionalOptions, $this->buttonOptions);
-                $icon = Html::tag('span', '', ['class' => "fa fa-$iconName"]);//update font awesome
+                $icon = Html::tag('span', '', ['class' => "fas fa-$iconName"]);//update font awesome
                 return Html::a($icon, $url, $options);
             };
         }
